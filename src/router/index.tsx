b2dd1008067b373f54c5c94ba4b22b64ea97a7f3
zhoufeiyu <119775549@qq.com>
2019-06-03 19:39:28 +0800

    fix bug

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,19 +1,18 @@
-import { Router, Switch, Route, Redirect } from "react-router-dom";
-import { history } from './history'
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import React, { Suspense, lazy } from "react";
 
 const Resume = lazy(() => import('../views/resume'))
 
 const RootRouter = () => {
   return (
-    <Router history={history}>
+    <HashRouter>
       <Suspense fallback={<div>loading</div>}>
         <Switch>
           <Route exact path="/" component={Resume} />
           <Redirect from="**" to="/"  />
         </Switch>
       </Suspense>
-    </Router>
+    </HashRouter>
   )
 }
 
--- src/views/resume/skill.tsx
+++ src/views/resume/skill.tsx
@@ -8,7 +8,7 @@ export default () => {
       <ol>
         <li>熟悉HTML5,CSS3,JavaScript,了解浏览器工作原理。</li>
         <li>熟悉Vue及全家桶，了解React及Angular，能根据需求独立开发组件。</li>
-        <li>了解TyceScript。</li>
+        <li>了解TypeScript。</li>
         <li>了解打包工具Webpack，了解版本控制工具Git。</li>
         <li>了解Python及后端框架django。</li>
       </ol>
