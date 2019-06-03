116a8b7a86bca7d1cea028fab4d60cccc70289bb
zhoufeiyu <119775549@qq.com>
2019-06-03 19:42:15 +0800

    text

--- src/views/resume/skill.tsx
+++ src/views/resume/skill.tsx
@@ -7,7 +7,7 @@ export default () => {
       <h1>技能</h1>
       <ol>
         <li>熟悉HTML5,CSS3,JavaScript,了解浏览器工作原理。</li>
-        <li>熟悉Vue及全家桶，了解React及Angular，能根据需求独立开发组件。</li>
+        <li>熟悉Vue及其全家桶，了解React及Angular，能根据需求独立开发组件。</li>
         <li>了解TypeScript。</li>
         <li>了解打包工具Webpack，了解版本控制工具Git。</li>
         <li>了解Python及后端框架django。</li>
